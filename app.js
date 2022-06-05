e71bba63961c96b89ddac3193f58c068bb32e222
Dereje <getutadessedereje@icloud.com>
2022-06-05 13:47:14 +0200

    Fix: probleme couleur qui ne match pas avec la couleur hexadecimal

--- app.js
+++ app.js
@@ -2,33 +2,39 @@ const body = document.querySelector('body');
 const copyHex = document.querySelector('#copy');
 const generate = document.querySelector('#generate');
 const hexCode = document.querySelector('.hexCode');
-
+const letters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
 copyHex.addEventListener('click', copyColor);
 
 generate.addEventListener('click', () => {
-    hexCode.textContent = getRandomColor();
-    body.style.backgroundColor = getRandomColor();
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[getRandomNmbr()];
+    }
+    hexCode.textContent = color;
+    body.style.backgroundColor = color;
 });
+
+
+
 
 body.style.backgroundColor = getRandomColor();
 hexCode.textContent = getRandomColor();
 
+
 function getRandomColor() {
-    let numbers = '0123456789ABCDEF';
-    let tags = '#';
+    let color = '#';
     for (let i = 0; i < 6; i++) {
-        tags += numbers[Math.floor(Math.random() * 16)];
+        color += letters[getRandomNmbr()];
     }
-    return tags;
+    return color;
+}
+
+function getRandomNmbr() {
+    return Math.floor(Math.random() * letters.length);
 }
 
 
 function copyColor() {
     document.execCommand('copy');
-    // set time out 1s to remove the message after copy
     hexCode.textContent = 'Hex code Copied !';
-    setTimeout(() => {
-        hexCode.textContent = getRandomColor();
-    }, 1000);
-
 }
