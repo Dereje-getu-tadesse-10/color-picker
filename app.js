d3d48bec42e68e33a6f19fc780c63a41d156ff13
Dereje <getutadessedereje@icloud.com>
2022-06-05 12:51:31 +0200

    Update: ajout fonction copy

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const body = document.querySelector('body');
-const copy = document.querySelector('#copy');
+const copyHex = document.querySelector('#copy');
 const generate = document.querySelector('#generate');
+const hexCode = document.querySelector('.hexCode');
 
+copyHex.addEventListener('click', copyColor);
 
 generate.addEventListener('click', () => {
+    hexCode.textContent = getRandomColor();
     body.style.backgroundColor = getRandomColor();
 });
 
 body.style.backgroundColor = getRandomColor();
+hexCode.textContent = getRandomColor();
 
 function getRandomColor() {
     let numbers = '0123456789ABCDEF';
     let tags = '#';
-    for (var i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
         tags += numbers[Math.floor(Math.random() * 16)];
     }
     return tags;
+}
+
+
+function copyColor() {
+    document.execCommand('copy');
 }
