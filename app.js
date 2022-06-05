35bac291172ba7bdf6a8acd698225a64cc729363
Dereje <getutadessedereje@icloud.com>
2022-06-05 12:12:10 +0200

    Update: modification de la fonction

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const body = document.querySelector('body');
+const copy = document.querySelector('#copy');
+const generate = document.querySelector('#generate');
 
-const colors = [];
-for (let i = 0; i < 100; i++) {
-    colors.push('#' + Math.floor(Math.random() * 16777215).toString(16));
-}
 
-function getRandomColor() {
-    return colors[Math.floor(Math.random() * colors.length)];
-}
+generate.addEventListener('click', () => {
+    body.style.backgroundColor = getRandomColor();
+});
 
 body.style.backgroundColor = getRandomColor();
+
+function getRandomColor() {
+    let numbers = '0123456789ABCDEF';
+    let tags = '#';
+    for (var i = 0; i < 6; i++) {
+        tags += numbers[Math.floor(Math.random() * 16)];
+    }
+    return tags;
+}
